e8857348b75013ed6c1ffbdaa35f6d884a28ad3d
ERIC CHANG <jason3067812@gmail.com>
2022-09-28 20:52:42 -0400

    init

---
+++ src/columbia_student_resource.py
@@ -0,0 +1,45 @@
+import pymysql
+
+import os
+
+
+class ColumbiaStudentResource:
+
+    def __int__(self):
+        pass
+
+    @staticmethod
+    def _get_connection():
+
+        #usr = os.environ.get("DBUSER")
+        #pw = os.environ.get("DBPW")
+        #h = os.environ.get("DBHOST")
+
+        # usr = "root"
+        # pw = "12234"
+        # h = "localhost"
+
+        usr = "admin"
+        pw = "eric30678"
+        h = "test.cvwwgxdyhdlt.us-east-2.rds.amazonaws.com"
+
+        conn = pymysql.connect(
+            user=usr,
+            password=pw,
+            host=h,
+            cursorclass=pymysql.cursors.DictCursor,
+            autocommit=True
+        )
+        return conn
+
+    @staticmethod
+    def get_by_key(key):
+
+        sql = "SELECT * FROM f22_databases.columbia_students where guid=%s";
+        conn = ColumbiaStudentResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=key)
+        result = cur.fetchone()
+
+        return result
+
